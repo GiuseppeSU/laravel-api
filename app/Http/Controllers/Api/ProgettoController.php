17c76b5c20a1a649480dc26947aef5a2ab754d65
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-05-31 19:20:26 +0200

    Fine esercizio laravel

--- app/Http/Controllers/Api/ProgettoController.php
+++ app/Http/Controllers/Api/ProgettoController.php
@@ -10,12 +10,22 @@ class ProgettoController extends Controller
 {
     public function index()
     {
-        $progetti = Progetto::with('type', 'technologies')->get();
+        $progetti = Progetto::with('type', 'technologies')->paginate(3);
         return response()->json([
             'success' => true,
             'results' => $progetti
 
 
+        ]);
+
+    }
+
+    public function show($slug)
+    {
+        $progetto = Progetto::where('slug', $slug)->with('type', 'technologies')->first();
+        return response()->json([
+            'succes' => true,
+            'progetto' => $progetto
         ]);
 
     }
